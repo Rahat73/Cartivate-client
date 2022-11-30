f831e77a1b85246195c97d10e0ea8ecd5b228c29
Rahat73 <55106915+Rahat73@users.noreply.github.com>
2022-12-01 03:21:07 +0600

    product card updated

--- src/Pages/Products/ProductCard/ProductCard.js
+++ src/Pages/Products/ProductCard/ProductCard.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FaCheckCircle } from "react-icons/fa";
+import { format } from 'date-fns'
+import { parseISO } from 'date-fns/esm';
 
 const ProductCard = ({ product }) => {
     return (
@@ -7,7 +10,23 @@ const ProductCard = ({ product }) => {
                 <figure><img src={product.image} alt="product" /></figure>
                 <div className="card-body">
                     <h2 className="card-title">{product.title}</h2>
-                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <p>{product.description}</p>
+                    <div className='flex justify-evenly'>
+                        <div className='font-semibold'>Price: {product.price}$</div>
+                        <div className='font-semibold'>Original price: {product.originalPrice}$</div>
+                    </div>
+                    <div className="flex justify-center items-center">
+                        <div className='font-semibold text-lg'>Seller: {product.sellerName}</div>
+                        {
+                            product.verified &&
+                            <span><FaCheckCircle className='text-md text-blue-600 ml-1'></FaCheckCircle></span>
+                        }
+                    </div>
+                    <div className='flex justify-evenly'>
+                        <div className='font-semibold'>Location: {product.location}</div>
+                        <div className='font-semibold'>Purchage year: {product.yearPurchase}</div>
+                    </div>
+                    <div>Posted On: {format(parseISO(product.postTime), 'PP')}</div>
                     <div className="card-actions justify-end">
                         <button className="btn btn-primary">Book Now</button>
                     </div>
